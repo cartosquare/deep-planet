4940d4702d515a1eff8fec42bda9070ce1764698
XuXiang <xux@geohey.com>
2017-03-06 13:43:06 +0800

    添加训练数据部署脚本

---
+++ pretrain_scripts/deploy.py
@@ -0,0 +1,51 @@
+import config
+import os
+import shutil
+
+if not os.path.exists(config.deploy_dir):
+    os.mkdir(config.deploy_dir) 
+
+training_dir = '%s/training_set' % (config.deploy_dir)
+if not os.path.exists(training_dir):
+    os.mkdir(training_dir) 
+
+
+train_txt = '%s/train.txt' % training_dir
+test_txt = '%s/test.txt' % training_dir
+
+ftrain = open(train_txt, 'w')
+ftest = open(test_txt, 'w')
+
+for ele in config.deploy:
+    print 'process data set ', ele
+
+    data_dir = '%s/training_set/%s' % (config.deploy_dir, ele)
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+
+    # train dir
+    train_dir = 'training_set/%s/valid_tif_tiles' % (ele)
+    new_train_dir = '%s/valid_tif_tiles' % (data_dir)
+
+    shutil.copytree(train_dir, new_train_dir)
+
+    # label dir 
+    label_dir = 'training_set/%s/labels' % (ele)
+    new_label_dir = '%s/labels' % (data_dir)
+
+    shutil.copytree(label_dir, new_label_dir)
+
+    # train file
+    train_file = 'train_%s.txt' % (ele)
+    with open(train_file, 'r') as f:
+        for line in f:
+            ftrain.write(line)
+    
+    # test file
+    test_file = 'test_%s.txt' % (ele)
+    with open(test_file, 'r') as f:
+        for line in f:
+            ftest.write(line)
+
+ftrain.close()
+ftest.close()
