8856157a979ebb206073519fe7d50faf5e51a264
XuXiang <xux@geohey.com>
2017-05-12 22:21:50 +0800

    可以输出每一类的精度图表

--- src/plot_training_process.py
+++ src/plot_training_process.py
@@ -6,35 +6,56 @@ import sys
 
 iterations = []
 acc = []
+loss = []
+class_index = 0
+if len(sys.argv) > 2:
+    class_index = int(sys.argv[2])
+
 with open(sys.argv[1]) as f:
     count = 0
     for line in f:
         items = line.strip().split()
+        hasx = False
+        hasacc = False
+        hasloss = False
         if len(items) > 5 and items[4] == 'Iteration' and items[6] == 'loss':
             print('iteration', items[5].split(',')[0])
             iterations.append(int(items[5].split(',')[0]))
-
+	    hasx = True
             count = count + 1
-        if len(items) > 10 and items[4] == 'Train' and items[5] == 'net' and items[6] == 'output' and items[7] == '#0:' and items[8] == 'accuracy' and items[9] == '=':
+
+        if len(items) > 10 and items[4] == 'Train' and items[5] == 'net' and items[6] == 'output' and items[7] == ('#%d:' % class_index) and items[9] == '=':
             print('acc', items[10])
             acc.append(float(items[10]))
-
+            hasacc = True
+        
+        if len(items) > 10 and items[4] == 'Train' and items[5] == 'net' and items[6] == 'output' and items[7] == '#1:' and items[8] == 'loss' and items[9] == '=':
+            print('loss', items[10])
+            loss.append(float(items[10]))
+            hasloss = True
             count = count + 1
         
 
 if (len(iterations) != len(acc)):
     print(len(iterations), len(acc))
     print('not consistent!!')
-    #exit()
-
-
-with open('plot.csv', 'w')as f:
-    f.write('#iter,accuracy\n')
-
-    for i in range(len(iterations)):
-        f.write('%s,%s\n' % (iterations[i], acc[i]))
-f.close()
+    if len(iterations) > len(acc):
+	iterations.pop()
+    else:
+	acc.pop()
+	
+if (len(iterations) != len(loss)):
+    print(len(iterations), len(loss))
+    print('not consistent!!')
+    if len(iterations) > len(loss):
+        iterations.pop()
+    else:
+        loss.pop()
 
 plt.figure(1)
 plt.plot(iterations, acc)
-plt.savefig('plot.png')
+plt.savefig('plot_acc_%d.png' % (class_index))
+
+plt.figure(2)
+plt.plot(iterations, loss)
+plt.savefig('plot_loss.png')
